23b4048e56e0f3e0f40a66a7f5b793edeb82452d
Leonardo Bonifacio Neves <lbonifacioneves@gmail.com>
2024-10-13 19:10:55 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
+import { CdkDragDrop, DragDropModule, moveItemInArray } from '@angular/cdk/drag-drop';
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NbThemeModule, NbLayoutModule, NbButtonModule } from '@nebular/theme';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NbLayoutModule, NbButtonModule, DragDropModule, NgFor],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  providers: [
+    NbThemeModule.forRoot({ name: 'default' }).providers!
+  ]
 })
 export class AppComponent {
-  title = 'PE_Nebular';
+  items = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine','Seven', 'Eight', 'Nine','Seven', 'Eight', 'Nine'];
+
+  drop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.items, event.previousIndex, event.currentIndex);
+  }
 }
